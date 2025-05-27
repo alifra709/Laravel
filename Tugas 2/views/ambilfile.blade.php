bae277623c7cd4c8c31660571954b7b475b7f361
Alif R <113462662+alifra709@users.noreply.github.com>
2025-05-27 23:59:54 +0700

    Delete Tugas 2 directory

--- Tugas 2/views/ambilfile.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Memanggil File dari Views tag heading1</h1>
-<h2>Memanggil File dari Views tag heading2</h2>
-<h3>Memanggil File dari Views tag heading3</h3>
-<h4>Memanggil File dari Views tag heading4</h4>
-<h5>Memanggil File dari Views tag heading5</h5>
-<h6>Memanggil File dari Views tag heading6</h6>
--- Tugas 2/views/v_html/getlorem.blade.php
+++
@@ -1,9 +0,0 @@
-<p>
-    <b>Mengenal Huruf tebal</b><br><br>TEST 1
-</p>
-<p>
-    <u>Mengenal Huruf garis bawah</u><br><br>TEST 2
-</p>
-<p>
-    <i>Mengenal Huruf miring</i>TEST 3
-</p>
